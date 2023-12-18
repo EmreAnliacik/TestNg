b3740edbca29fc3334484497965b09718169001c
EmreAnliacik <emreanliacik@hotmail.com>
2023-12-18 13:55:05 +0100

    TestNG

--- src/Gun01/_01_Giris.java
+++ src/Gun01/_01_Giris.java
@@ -1,4 +1,37 @@
 package Gun01;
 
+import org.testng.annotations.Test;
+
 public class _01_Giris {
+
+    //Default olarak alfabetik siraya gore calisir
+
+
+
+    @Test(priority = 1)
+    public void driverIslemleriap() {
+        System.out.println("driverIslemleriniYap");
+
+    }
+
+    @Test(priority = 2)
+    public void webSitesiniAc() {
+        System.out.println("Web sitesini Ac");
+
+    }
+
+    @Test(priority = 3)
+    public void LoginTestiniYap() {
+        System.out.println("LoginTestiniYap");
+
+    }
+
+
+    @Test(priority = 4)
+    public void driverKapat() {
+        System.out.println("driverKapat");
+
+    }
+
+
 }
