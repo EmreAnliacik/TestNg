5f34f0361fc0f1315b40390e1b3d4111993073db
EmreAnliacik <emreanliacik@hotmail.com>
2023-12-25 15:53:17 +0100

    TestNG

--- src/Utility/MyFuction.java
+++ src/Utility/MyFuction.java
@@ -1,5 +1,9 @@
 package Utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 public class MyFuction {
 
     public static void Wait(int sn){
@@ -9,6 +13,11 @@ public class MyFuction {
         catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void successMessageValidation(){
+        WebElement chechMessage = BaseDriver.driver.findElement(By.cssSelector("[class='alert alert-success alert-dismissible']"));
+        Assert.assertEquals((chechMessage.getText()), "Success: Your newsletter subscription has been successfully updated!");
     }
 
 
